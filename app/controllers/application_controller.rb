7eb9c944f0f6b6010fdf8fc6095bbf691ccd5c89
nans64 <nans.noel@gmail.com>
2019-03-11 20:21:42 +0100

    Clean files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,32 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
   #before_action :browser_locale(current_user)
-  browser = Browser.new("Some User Agent", accept_language: "en-us")
 
   def set_locale
     I18n.locale = extract_locale_from_tld || I18n.default_locale
   end
 
-  browser.
-# Get locale from top-level domain or return +nil+ if such locale is not available
-# You have to put something like:
-#   127.0.0.1 application.com
-#   127.0.0.1 application.it
-#   127.0.0.1 application.pl
-# in your /etc/hosts file to try this out locally
+
   def extract_locale_from_tld
-    parsed_locale = request.host.split('.').last
+    parsed_locale = request.env['HTTP_ACCEPT_LANGUAGE'][0..1]
     I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
   end
 
-  def default_url_options(options = {})
-    { locale: I18n.locale }.merge options
+  def default_url_options
+    { locale: I18n.locale }
   end
 
-  def browser_locale(request)
-    locales = request.env['HTTP_ACCEPT_LANGUAGE'] || ""
-    locales.scan(/[a-z]{2}(?=;)/).find do |locale|
-      I18n.available_locales.include?(locale.to_sym)
-    end
-  end
 end
