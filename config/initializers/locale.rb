e2510f09a5a905606e5a3bf83506b7b4adc00f42
nans64 <nans.noel@gmail.com>
2019-03-11 20:23:03 +0100

    Add config files for translation

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -5,4 +5,4 @@ I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
 I18n.available_locales = [:en, :fr]
 
 # Set default locale to something other than :en
-I18n.default_locale = :fr
+I18n.default_locale = :en
