686d1f34cb66b068c1f491bb9096ae05e350d284
nans64 <nans.noel@gmail.com>
2019-03-12 13:29:56 +0100

    Quick fix for push test

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_094058) do
+ActiveRecord::Schema.define(version: 2019_03_12_111441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -74,6 +95,19 @@ ActiveRecord::Schema.define(version: 2019_03_12_094058) do
     t.index ["participation_id"], name: "index_proofs_on_participation_id"
   end
 
+  create_table "services", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "provider"
+    t.string "uid"
+    t.string "access_token_secret"
+    t.string "refresh_token"
+    t.datetime "expires_at"
+    t.text "auth"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_services_on_user_id"
+  end
+
   create_table "user_send_dares", force: :cascade do |t|
     t.bigint "dare_id"
     t.bigint "sender_id"
@@ -97,9 +131,18 @@ ActiveRecord::Schema.define(version: 2019_03_12_094058) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "videos", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "dares", "categories"
   add_foreign_key "difficulty_ratings", "dares"
   add_foreign_key "notifications", "users"
   add_foreign_key "participations", "users"
   add_foreign_key "proofs", "participations"
+  add_foreign_key "services", "users"
 end
