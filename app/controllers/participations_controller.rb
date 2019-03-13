39a5a3d28897c1de2e556aeb41b3b64efe36ff97
dany-about <dany.about@edu.escpeurope.eu>
2019-03-13 14:31:48 +0100

    AJAX update participations

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,11 +1,14 @@
 class ParticipationsController < ApplicationController
 
   def create 
-    Participation.create!(user: current_user, dare: Dare.find(params[:dare])) 
-    current_user.notify_followers(Dare.find(params[:dare]), "participation_created")
+    @participation = Participation.create!(user: current_user, dare: Dare.find(params[:dare])) 
+    current_user.notify_followers(@participation, "participation_created")
   end
 
   def update
+    @participation = Participation.find_by(user: current_user, dare:Dare.find(params[:dare]))
+    @participation.is_achieved = nil
+    current_user.notify_followers(@participation, "participation_achieved")
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_13_010557) do
+ActiveRecord::Schema.define(version: 2019_03_13_103317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
