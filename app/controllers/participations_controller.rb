aedbf2ddb42c511b4f2c47b249f24da881c84ad3
dany-about <dany.about@edu.escpeurope.eu>
2019-03-14 16:54:48 +0100

    added already participating and already achieved buttons to _description

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -8,7 +8,7 @@ class ParticipationsController < ApplicationController
   end
 
   def update
-    @participation = Participation.find(params[:id]))
+    @participation = Participation.find(params[:id])
     @participation.is_achieved = nil
     current_user.notify_followers(@participation, "participation_achieved")
   end
