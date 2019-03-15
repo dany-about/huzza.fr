f85a96cf52579d505ad900957b1381e1a01d4359
dany-about <dany.about@edu.escpeurope.eu>
2019-03-15 19:06:09 +0100

    RAS

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,10 +1,12 @@
 class ParticipationsController < ApplicationController
-  respond_to :html, :js
   
   def create 
     if Participation.find_by(user: current_user, dare: Dare.find(params[:dare])) == nil
       @participation = Participation.create!(user: current_user, dare: Dare.find(params[:dare])) 
       current_user.notify_followers(@participation, "participation_created")
+    end
+    respond_to do |format|
+      format.js {render "news/index"}
     end
   end
 
--- app/controllers/star_dares_controller.rb
+++ app/controllers/star_dares_controller.rb
@@ -4,6 +4,9 @@ class StarDaresController < ApplicationController
     if StarDare.find_by(dare: Dare.find(params[:dare]), user: current_user) == nil
       StarDare.create!(dare: Dare.find(params[:dare]), user: current_user)
     end
+    respond_to do |format|
+      format.js {redirect_to "news/index"}
+    end
   end
   
   def destroy
