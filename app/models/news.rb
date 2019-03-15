a48e2c5d1401b6abb187f17116ba6ac1214e149b
dany-about <dany.about@edu.escpeurope.eu>
2019-03-15 12:38:58 +0100

    _dare_card

--- app/models/news.rb
+++ app/models/news.rb
@@ -4,6 +4,6 @@ class News < ApplicationRecord
   belongs_to :event, polymorphic: true
   # L'association polymorphic permet d'avoir une colonne event_type = "Dare" ou "Participation" et une colonne event_id 
   
-  validates_inclusion_of :occasion, in: %w( participation_created participation_achieved achievement_rejected dare_created dare_sent dare_accepted ), on: :create, message: "Occasion %s is not included in the list" 
+  # validates_inclusion_of :occasion, in: %w( participation_created participation_achieved achievement_rejected dare_created dare_sent dare_accepted ), on: :create, message: "Occasion %s is not included in the list" 
 
 end
