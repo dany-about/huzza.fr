8f916ce33034562fd553b987628304ad311c4ee9
dany-about <dany.about@edu.escpeurope.eu>
2019-03-16 01:01:46 +0100

    AJAX friend requests and follows, also btn order fix

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,14 +1,21 @@
 class FollowsController < ApplicationController
-
+  respond_to :html, :js
+  
   def create    
     if Follow.find_by(user: User.find(params[:user]), follower: current_user) == nil
       Follow.create!(user: User.find(params[:user]), follower: current_user)
+    end
+    respond_to do |format|
+      format.js {render template: 'users/show'}
     end
   end
   
   def destroy
     if Follow.exists?(params[:id])
       Follow.find(params[:id]).destroy
+    end
+    respond_to do |format|
+      format.js {render template: 'users/show'}
     end
   end
 
