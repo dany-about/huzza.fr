fbbe1e6b48fdc568d4986f071599c1386efc1e83
dany-about <dany.about@edu.escpeurope.eu>
2019-03-17 20:48:24 +0100

    comments create and destroy in AJAX

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,12 +5,13 @@ class CommentsController < ApplicationController
     params[:type].constantize.find(params[:commentable_id]).comments.create!(user: current_user, content: params[:content])
   end
 
-  def update
-
-  end
-
   def destroy
-
+    if Comment.exists?(params[:id])
+      Comment.find(params[:id]).destroy
+    end
+    respond_to do |format|
+      format.js {render :create}
+    end
   end
 
 end
