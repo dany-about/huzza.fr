649fb54b6dd819e71f43b6acbb4b77948fa42b16
dany-about <dany.about@edu.escpeurope.eu>
2019-03-18 20:58:54 +0100

    configured menu for the feed

--- app/models/news.rb
+++ app/models/news.rb
@@ -2,8 +2,7 @@ class News < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: "User"
   belongs_to :event, polymorphic: true
-  # L'association polymorphic permet d'avoir une colonne event_type = "Dare" ou "Participation" et une colonne event_id 
   
-  # validates_inclusion_of :occasion, in: %w( participation_created participation_achieved achievement_rejected dare_created dare_sent dare_accepted ), on: :create, message: "Occasion %s is not included in the list" 
+  validates_inclusion_of :occasion, in: %w( participation_created participation_achieved achievement_rejected dare_created dare_sent dare_received dare_accepted ), on: :create, message: "Occasion %s is not included in the list" 
 
 end
