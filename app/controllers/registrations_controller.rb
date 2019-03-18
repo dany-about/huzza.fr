3b6c411f74cbbc1affae6c4a07fd83efc2c35c06
nikkiHelloThp <nicolas.stanojevic.lpo@gmail.com>
2019-03-18 13:11:49 +0100

    registration controller fix conflict

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,12 @@
 class RegistrationsController < ApplicationController
 	
+
+end
+
+
+
+class RegistrationsController < Devise::RegistrationsController
+
 	def after_sign_up_path_for(resource)
 	  after_sign_in_path_for(resource)
 	end
@@ -8,11 +15,6 @@ class RegistrationsController < ApplicationController
 	  user_path
 	end
 
-end
-
-
-
-class RegistrationsController < Devise::RegistrationsController
 
 	def create
 		build_resource(sign_up_params)
