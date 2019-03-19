19e0f80b2342979115106e8ee84636a2eed7d4d2
dany-about <dany.about@edu.escpeurope.eu>
2019-03-19 23:59:48 +0100

    fixed orthograph

--- app/models/accomplishment.rb
+++ app/models/accomplishment.rb
@@ -1,2 +1,4 @@
 class Accomplishment < ApplicationRecord
+  has_many :user_accomplishments
+  has_many :users, through: :user_accomplishments
 end
--- db/migrate/20190315010409_create_user_accomplishements.rb
+++
@@ -1,9 +0,0 @@
-class CreateUserAccomplishements < ActiveRecord::Migration[5.2]
-  def change
-    create_table :user_accomplishements do |t|
-      t.belongs_to :user
-      t.belongs_to :accomplishment
-      t.timestamps
-    end
-  end
-end
