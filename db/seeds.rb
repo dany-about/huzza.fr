e5e1eb22247b70c89e4e89920a6dd22e68183693
dany-about <dany.about@edu.escpeurope.eu>
2019-03-20 17:22:53 +0100

    RAS

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+FriendRequest.destroy_all
 Accomplishment.destroy_all
 DifficultyRating.destroy_all
 Reaction.destroy_all
