fcdcf58ade300dcdd0b614c6b790f60efcbfd217
dany-about <dany.about@edu.escpeurope.eu>
2019-03-21 12:05:48 +0100

    cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,25 +9,9 @@ Rails.application.routes.draw do
   post '/sign_up_validation', to: 'users/omniauth_callbacks#sign_up_validation'
 
   # Avatar routes
-  resources :users do
+  resources :users, only: [:create] do
     resources :avatars, only: [:create]
   end
-
-  # Avatar routes
-  resources :participations do
-    resources :pictures, only: [:create]
-  end
-
-  # Avatar routes
-  resources :participations do
-    resources :videos
-  end
-
-  # Need to be deleted
-  get 'home/test_card'
-  get 'home/testdany'
-  get 'home/chantier_card'
-  get 'users/showtest'
 
   get 'home/team'
   get 'home/howitworks'
@@ -35,22 +19,21 @@ Rails.application.routes.draw do
 
   # Home page
   root 'news#index'
-  
 
   resources :follows, only: [:create, :destroy]
   resources :friend_requests, only: [:create, :update, :destroy]
   
   resources :dares, except: [:show, :edit, :update, :destroy]
-  resources :participations, only: [:create, :update]
+  resources :participations, only: [:create, :update] do 
+    resources :pictures, only: [:create]
+    resources :videos, only: [:create]
+  end
   resources :news, only: [:index]
 
   resources :user_send_dares, only: [:create, :update]
   resources :star_dares, only: [:create, :destroy]
   resources :difficulty_ratings, only: [:create]
   resources :reactions, only: [:create, :destroy]
-  resources :comments, only: [:create, :update, :destroy]
-
-  # Dares route
-  resources :dares
+  resources :comments, only: [:create, :destroy]
 
 end
