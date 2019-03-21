4220dd87a2bba7573b6a38c721e6803812951a24
dany-about <dany.about@edu.escpeurope.eu>
2019-03-21 16:32:17 +0100

    contestation creation fixed, put proof_list in a partial for easy AJAX call

--- app/controllers/contestations_controller.rb
+++ app/controllers/contestations_controller.rb
@@ -2,7 +2,7 @@ class ContestationsController < ApplicationController
   respond_to :js
 
   def create
-    if Contestation.find_by(user: current_user, participation_id: params[:id]) != nil
+    if Contestation.find_by(user: current_user, participation_id: params[:id]) == nil
       Contestation.create!(user: current_user, participation_id: params[:id])
     end
   end
