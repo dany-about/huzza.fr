da1099c9023e4939df2407c0e3932601d31c97d2
dany-about <dany.about@edu.escpeurope.eu>
2019-03-22 13:06:00 +0100

    rewired link_to landing instead of registration#new, added redirect after sign up to home controller, deleted useless registration#new view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
+  def after_sign_up_path_for(resource)
+		'dares#index'
+  end
+  
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,6 @@ class RegistrationsController < Devise::RegistrationsController
 	protected
 
 	def after_sign_up_path_for(resource)
-		'dares#index' # Or :prefix_to_your_route
+		'dares#index'
 	end
 end
