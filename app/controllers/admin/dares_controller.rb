5413eaf84e9df9fa23225b8d3dd0b09952db7cba
dany-about <dany.about@edu.escpeurope.eu>
2019-03-24 18:44:24 +0100

    created admin

---
+++ app/controllers/admin/dares_controller.rb
@@ -0,0 +1,14 @@
+class Admin::DaresController < ApplicationController
+  before_action :is_admin?
+
+  def index
+  end
+
+  def destroy
+  end
+
+  def is_admin?
+    redirect_to "/" unless current_user.is_admin
+  end
+
+end
---
+++ app/helpers/admin/dares_helper.rb
@@ -0,0 +1,2 @@
+module Admin::DaresHelper
+end
