df207de0337b135cd30f6b45ecf980a5138bd54e
dany-about <dany.about@edu.escpeurope.eu>
2019-04-02 18:34:42 +0200

    added destruction of dependencies

--- app/models/dare.rb
+++ app/models/dare.rb
@@ -1,27 +1,27 @@
 class Dare < ApplicationRecord
   belongs_to :creator, class_name: "User"
   belongs_to :category
-  has_many :difficulty_ratings
+  has_many :difficulty_ratings, dependent: :destroy
 
   # Participations
-  has_many :participations
+  has_many :participations, dependent: :destroy
   has_many :participants, through: :participations, source: :user
-  has_many :reactions, through: :participations
+  has_many :reactions, through: :participations, dependent: :destroy
 
   # Sending and receiving Dares
-  has_many :user_send_dares
+  has_many :user_send_dares, dependent: :destroy
   has_many :senders, through: :user_send_dares, source: :user, foreign_key: "sender_id"
   has_many :recipients, through: :user_send_dares, source: :user, foreign_key: "recipient_id"
   
   # Starring Dares
-  has_many :star_dares
+  has_many :star_dares, dependent: :destroy
   has_many :starrers, through: :star_dares, source: :user
 
   # Polymorphic associations for News, Comments & Reactions
-  has_many :news, as: :event
-  has_many :comments, as: :commentable
+  has_many :news, as: :event, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
   has_many :participation_comments, through: :participations, source: :comments
-  has_many :reactions, as: :reactionable
+  has_many :reactions, as: :reactionable, dependent: :destroy
 
   # Validations
   validates :description, presence: true, length: { in: 30..1000 }
